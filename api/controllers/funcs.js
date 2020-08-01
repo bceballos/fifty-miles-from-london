be22c394653c23c2b42026ea3753b29263eb2d2d
Brennan Ceballos Peña <brennanpena@hotmail.co.uk>
2020-08-01 17:56:14 +0100

    ADDED: Haversine function logic

--- api/controllers/funcs.js
+++ api/controllers/funcs.js
@@ -1,26 +1,30 @@
 'use strict';
+    module.exports = {}
 
-module.exports = function() {
-    return {
-        /*
-         * GET the JSON object from the end point
-         */
-        get(url) {
+    function haversine(lat, long) {
+        // All Latitudes and Longitudes converted to radians
+        // Latitude and Longitude of London gotten from https://www.latlong.net/place/london-the-uk-14153.html
+        const londonLat = 51.509865;
+        const londonLong = -0.118092;
+        const toRadians = (Math.PI / 180);
 
-        }
+        // Difference between the points
+        const diffLat = (lat - londonLat) * toRadians;
+        const diffLong = (long - londonLong) * toRadians; 
 
-        /*
-         * Operate on the user latitude and longitude
-         */
-        haversine(lat, long) {
+        // Big R for Radius of the earth in miles
+        const R = 3958.8;
 
-        }
+        const a = Math.pow(Math.sin(diffLat / 2), 2) + Math.cos(londonLat * toRadians) * Math.cos(londonLong * toRadians) * Math.pow(Math.sin(diffLong / 2));
+
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c;
+    }
 
         /*
          * POST the JSON object or the errors
          */
-        post(userArray) {
-            
-        }
-    }
-}
+        // post(userArray) {
+
+        // }
