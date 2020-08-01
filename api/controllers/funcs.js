54c6e377da5d7f3c7c7d36c94a87239ccb3f31dc
Brennan Ceballos Peña <brennanpena@hotmail.co.uk>
2020-08-01 16:17:18 +0100

    ADDED: Skeleton for functions file

---
+++ api/controllers/funcs.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function() {
+    return {
+        /*
+         * GET the JSON object from the end point
+         */
+        get(url) {
+
+        }
+
+        /*
+         * Operate on the user latitude and longitude
+         */
+        haversine(lat, long) {
+
+        }
+
+        /*
+         * POST the JSON object or the errors
+         */
+        post(userArray) {
+            
+        }
+    }
+}
