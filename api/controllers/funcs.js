62b74fc8a164625b1fbd12b7bc2406322ccea71e
Brennan Ceballos Peña <brennanpena@hotmail.co.uk>
2020-08-01 18:03:21 +0100

    CHANGED: Math logic to be correct accidentally using longitude of london instead of londons latitude and user's latitude

--- api/controllers/funcs.js
+++ api/controllers/funcs.js
@@ -15,7 +15,7 @@
         // Big R for Radius of the earth in miles
         const R = 3958.8;
 
-        const a = Math.pow(Math.sin(diffLat / 2), 2) + Math.cos(londonLat * toRadians) * Math.cos(londonLong * toRadians) * Math.pow(Math.sin(diffLong / 2));
+        const a = Math.pow(Math.sin(diffLat / 2), 2) + Math.cos(londonLat * toRadians) * Math.cos(lat * toRadians) * Math.pow(Math.sin(diffLong / 2));
 
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
