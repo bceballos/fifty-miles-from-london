66a117450d6e7c0eda34333943099bb8b888b27f
Brennan Ceballos Peña <brennanpena@hotmail.co.uk>
2020-08-03 18:56:52 +0100

    CHANGED: URL and route

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/get']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-    console.log('try this:\ncurl http://127.0.0.1:' + port + "/get?name=https://bpdts-test-app.herokuapp.com/uers");
+  if (swaggerExpress.runner.swagger.paths['/returnUsers']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + "/get?name=https://bpdts-test-app.herokuapp.com/users");
   } else {
     console.log("Chief it don't");
   }
